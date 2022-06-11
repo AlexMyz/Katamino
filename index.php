dd8875c7b5b3e67b89eed1c845fb61a67f0e0c73
AlexMyz <60917617+AlexMyz@users.noreply.github.com>
2022-06-11 10:54:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+include "configs/db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Katamino</title>
+</head>
+<body>
+	
+</body>
+</html>
