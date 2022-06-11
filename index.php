6718da485f8d412d93bde92d8e4e93ce717f6ef5
Roman Melnychuk <roman4iek02@gmail.com>
2022-06-11 19:01:35 +0300

    finished start screen

--- index.php
+++ index.php
@@ -8,9 +8,19 @@ include "configs/db.php";
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Imprima&display=swap" rel="stylesheet">
 	<title>Katamino</title>
 </head>
 <body>
-	
+	<div class="greet-scr">
+		<p>Katamino</p>
+		<div class="log">
+			<a href="#" class="btn signin-btn">Sign In</a>
+			<a href="#" class="btn login-btn">Login</a>
+		</div>
+	</div>
 </body>
 </html>
