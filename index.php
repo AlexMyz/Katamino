8e3cce9cd0f1590248ff0e50a7cddecc66da4536
Roman Melnychuk <roman4iek02@gmail.com>
2022-06-12 11:56:22 +0300

    fixed bugs

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ include "configs/db.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Imprima&display=swap" rel="stylesheet">
+	
 	<title>Katamino</title>
 </head>
 <body>
@@ -25,7 +25,7 @@ include "configs/db.php";
 	<div class="greet-scr">
 		<p class="game-name">Katamino</p>
 		<div class="log">
-			<a href="#" class="btn signin-btn">Sign In</a>
+			<a href="#" class="btn signin-btn">Sign Up</a>
 			<a href="login-form.php" class="btn login-btn">Login</a>
 		</div>
 	</div>
